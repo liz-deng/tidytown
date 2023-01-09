b547caaf3e4a7bfa8eaf73ece1b7875541cffec7
elideng <elideng@uw.edu>
2023-01-09 13:27:11 -0800

    data initial

--- source/app_server.R
+++ source/app_server.R
@@ -10,7 +10,11 @@ library(mapproj)
 #install.packages("readxl")
 library("readxl")
 
-#--------------Data Wrangling---------------------------------
+#--------------Data Wrangling---------------------------
+sss_20 <- read_excel("tidytown/data/WA2020_SSS.xlsx", sheet = "By Family")
+# View(sss_20)
+
+
 
 
 
